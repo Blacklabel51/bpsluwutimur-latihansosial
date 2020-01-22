05543953c6afb9b9d7216433bed8233a4ef9196b
unknown <statistikceria@gmail.com>
2020-01-22 21:01:43 +0800

    ulang lg buat model

--- lib/authenticate/authenticate.dart
+++ lib/authenticate/authenticate.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sosial/screens/authenticate/register.dart';
-import 'package:sosial/screens/authenticate/sign_in.dart';
+import 'package:sosial/authenticate/register.dart';
+import 'package:sosial/authenticate/sign_in.dart';
 
 class Authenticate extends StatefulWidget {
   @override
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sosial/models/user.dart';
-import 'package:sosial/screens/authenticate/authenticate.dart';
+import 'package:sosial/authenticate/authenticate.dart';
 import 'package:sosial/screens/home/home.dart';
 //import 'package:sosial/screens/home/home.dart';
 
